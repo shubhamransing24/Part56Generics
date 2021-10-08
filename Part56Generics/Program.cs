ae421fb7cb712d37430b967aeebb0cc5f89497e1
priyasonar1207 <83229578+priyasonar1207@users.noreply.github.com>
2021-10-08 14:39:02 +0530

    Add project files.

---
+++ Part56Generics/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Part56Generics
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Calculator c = new Calculator();
+            bool IsEqual = c.Equals(1,10);
+            Console.WriteLine(IsEqual);
+            Calculators<string> s = new Calculators<string>();
+           bool stringE= s.StingEquality("Ram","Ram");
+            Console.WriteLine(stringE);
+        }
+        class Calculator
+        {
+            public bool Equals<T>(T  a, T b)
+            {
+                return a.Equals(b);
+            }
+        }
+        class Calculators<S>
+        {
+            public bool StingEquality(S a,S b )
+            {
+                return a.Equals(b);
+            }
+        }
+    }
+}
